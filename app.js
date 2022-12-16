216b55ca11ddad3d557b13efbb9e75efb29a73de
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-12-16 12:07:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-// const express = require("express")
-// const app = express()
+const express = require("express")
+const app = express()
 // const io = require("socket.io")(app)
-// const mongoose = require("mongoose")
-// const path = require("path")
+const mongoose = require("mongoose")
+const path = require("path")
 
-// mongoose.connect("mongodb+srv://mongoadmin:mongoadmin@cluster0.exxx1cv.mongodb.net/sathvik?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://mongoadmin:mongoadmin@cluster0.exxx1cv.mongodb.net/sathvik?retryWrites=true&w=majority")
 
-// mongoose.connection.on("open",()=>{
-//     console.log("Database connected");
-//     app.listen(process.env.PORT || 5000 , ()=>{console.log(`listening on port ${process.env.PORT || 5000} ...`)})
-// }).on("error",()=>{
-//     console.log("db error :(");
-// })
+mongoose.connection.on("open",()=>{
+    console.log("Database connected");
+    app.listen(process.env.PORT || 3000 , ()=>{console.log(`listening on port ${process.env.PORT || 3000} ...`)})
+}).on("error",()=>{
+    console.log("db error :(");
+})
 
-// app.use(express.static( path.join(__dirname , "static")))
-
-
-// app.get("/about",(req,resp)=>{
-//     resp.sendFile("about.html" , {root : path.join(__dirname , "static") })
-// })
-
-// app.get("/",(req,resp)=>{
-//     resp.sendFile("index.html" , {root : path.join(__dirname , "static")})
-// })
+app.use(express.static( path.join(__dirname , "static")))
 
 
-// app.use((req,resp)=>{
-//     resp.send("your requested url is incorrect !")
-// })
+app.get("/about",(req,resp)=>{
+    resp.sendFile("about.html" , {root : path.join(__dirname , "static") })
+})
+
+app.get("/",(req,resp)=>{
+    resp.sendFile("index.html" , {root : path.join(__dirname , "static")})
+})
+
+
+app.use((req,resp)=>{
+    resp.send("your requested url is incorrect !")
+})
 
 
 
-console.log("this is how it works!")
+// console.log("this is how it works!")
 
